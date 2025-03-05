b7b7846b98336fa7a1709e432a2d536bc29f0ff1
Yul Jeon <dalbitstream@gmail.com>
2025-03-05 13:54:53 +0900

    feat: 클라이언트와 서버 연결

--- chat-client/src/app/page.tsx
+++ chat-client/src/app/page.tsx
@@ -1,4 +1,21 @@
+"use client";
+
+import { useEffect } from "react";
+import { Client } from "@stomp/stompjs";
+
 export default function Home() {
+  useEffect(() => {
+    const client = new Client({
+      brokerURL: "ws://localhost:8080/ws/chat",
+    });
+
+    client.onConnect = () => {
+      console.log("연결되었습니다!");
+    };
+
+    client.activate();
+  }, []);
+
   return (
     <div>
       <div>WebSocket</div>
